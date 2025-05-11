864dc5a9a4e60aab0a24a094b794e025954bdb28
SidraRaza <160983959+SidraRaza@users.noreply.github.com>
2025-05-11 13:48:08 +0500

    first commit

---
+++ oop/app.py
@@ -0,0 +1,16 @@
+# class Student:
+#     name="Sidra"
+
+# s1 = Student()
+# print(s1.name)
+
+# s2 = Student()
+# print(s2.name)
+
+class Car:
+    color = "blue"
+    model = "bmw"
+
+c1 = Car()
+print(c1.color)
+print(c1.model)
